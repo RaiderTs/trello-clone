2936064205c20f133b45f1ee28fcd200cc001cee
Alex <a.tsotsko@pinta.pro>
2024-01-30 23:03:21 +0200

    feat: adds navbar and footer components

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -12,8 +12,8 @@ const headingFont = localFont({
 
 const textFont = Poppins({
   subsets: ['latin'],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
-})
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
 
 const MarketingPage = () => {
   return (
@@ -31,11 +31,16 @@ const MarketingPage = () => {
         <h1 className='text-3xl md:text-6xl text-center text-neutral-800 mb-6'>
           Taskify helps team move
         </h1>
-        <div className='text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit'>
+        <div className='text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md w-fit'>
           work forward.
         </div>
       </div>
-      <div className={cn('text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto', textFont.className)}>
+      <div
+        className={cn(
+          'text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto',
+          textFont.className
+        )}
+      >
         Collaborate, manage projects, and reach new productivity peaks. From
         high rises to the home office, the way your team works is unique -
         accomplish it all with Taskify.
