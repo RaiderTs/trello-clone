8d679b3064b0c21e16e1dcee152e41e3d701ddaa
Alex <a.tsotsko@pinta.pro>
2024-02-11 15:03:15 +0200

    feat: adds shema file

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { create } from '@/actions/create-board';
+import { FormButton } from '@/app/(platform)/(dashboard)/organization/[organizationId]/form-button';
+import { FormInput } from '@/app/(platform)/(dashboard)/organization/[organizationId]/form-input';
+import { Button } from '@/components/ui/button';
+import { useFormState } from 'react-dom';
+
+export const Form = () => {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(create, initialState);
+
+  return (
+    <form action={dispatch}>
+      <div className='flex flex-col space-y-2'>
+        <FormInput errors={state?.errors} />
+      </div>
+      <FormButton />
+    </form>
+  );
+};
