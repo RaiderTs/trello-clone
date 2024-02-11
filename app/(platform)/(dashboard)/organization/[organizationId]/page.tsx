80e89a7a2d267143a6d78c07b2aff3eb8e27875e
Alex <a.tsotsko@pinta.pro>
2024-02-11 20:00:51 +0200

    feat: adds Info component

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,20 +1,9 @@
-import { create } from '@/actions/create-board';
-import { Board } from './board';
-import { Button } from '@/components/ui/button';
-import { db } from '@/lib/db';
-import { Form } from '@/app/(platform)/(dashboard)/organization/[organizationId]/form';
+import { Info } from '@/app/(platform)/(dashboard)/organization/[organizationId]/_components/info';
 
 const OrganizationIdPage = async () => {
-  const boards = await db.board.findMany();
-
   return (
-    <div className='flex flex-col space-y-4'>
-      <Form />
-      <div className='space-y-2'>
-        {boards.map((board) => (
-          <Board key={board.id} title={board.title} id={board.id} />
-        ))}
-      </div>
+    <div className='w-full mb-20'>
+      <Info isPro />
     </div>
   );
 };
