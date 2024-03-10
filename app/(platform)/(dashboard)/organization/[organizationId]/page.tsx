fd1b08c6149b253f89a01aed1ea176736dcf3e70
Alex <a.tsotsko@pinta.pro>
2024-03-10 17:15:27 +0200

    feat: adds board-list component

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,15 @@
+import { BoardList } from '@/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list';
 import { Info } from '@/app/(platform)/(dashboard)/organization/[organizationId]/_components/info';
+import { Separator } from '@/components/ui/separator';
 
 const OrganizationIdPage = async () => {
   return (
     <div className='w-full mb-20'>
       <Info isPro />
+      <Separator className={'my-4'} />
+      <div className='px-2 md:px-4'>
+        <BoardList />
+      </div>
     </div>
   );
 };
