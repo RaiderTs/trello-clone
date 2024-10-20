0368e7a8c3f9c1d35d8f2d9ed98f1986e113d1dd
Alex <a.tsotsko@pinta.pro>
2024-10-20 10:30:28 +0300

    feat: adds delete list action

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { ElementRef, useRef } from 'react';
+import { toast } from 'sonner';
 import { List } from '@prisma/client';
 import { MoreHorizontal, X } from 'lucide-react';
 
@@ -9,6 +11,8 @@ import {
   PopoverTrigger,
   PopoverClose,
 } from '@/components/ui/popover';
+import { useAction } from '@/hooks/use-action';
+import { deleteList } from '@/actions/delete-list';
 import { Button } from '@/components/ui/button';
 import { FormSubmit } from '@/components/form/form-submit';
 import { Separator } from '@/components/ui/separator';
@@ -19,6 +23,30 @@ interface ListOptionsProps {
 }
 
 export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
+
+  const closeRef = useRef<ElementRef<'button'>>(null); 
+
+  const { execute: executeDelete } = useAction(deleteList, {
+    onSuccess: (data) => {
+      toast.success(`List ${data.title} deleted`);
+      closeRef.current?.click();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onDelete = (formData: FormData) => {
+    const id = formData.get('id') as string;
+    const boardId = formData.get('boardId') as string;
+
+    executeDelete({ id, boardId });
+  };
+
+  if (!data) {
+    return null;
+  }
+
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -30,7 +58,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
         <div className='text-sm font-medium text-center text-neutral-600 pb-4'>
           List actions
         </div>
-        <PopoverClose asChild>
+        <PopoverClose ref={closeRef} asChild>
           <Button
             className='h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600'
             variant='ghost'
@@ -58,7 +86,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
 
         <Separator />
 
-        <form>
+        <form action={onDelete}>
           <input hidden name='id' id='id' value={data?.id} />
           <input hidden name='boardId' id='boardId' value={data?.boardId} />
           <FormSubmit
