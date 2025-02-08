bf8e6b47f15fe3d884ecd474fcd3770bb3f825c3
Alexander Tsotsko <alex.tsotsko21@gmail.com>
2025-02-08 22:58:28 +0200

    `feat: updated list-item component with new features and imports`

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -1,12 +1,13 @@
-'use client';
+"use client";
 
-import { ElementRef, useRef, useState } from 'react';
+import { ElementRef, useRef, useState } from "react";
 
-import { ListWithCards } from '@/types';
+import { ListWithCards } from "@/types";
 
-import { ListHeader } from './list-header';
-import { CardForm } from './card-form';
-
+import { cn } from "@/lib/utils";
+import { ListHeader } from "./list-header";
+import { CardForm } from "./card-form";
+import { CardItem } from "./card-item";
 
 interface ListItemProps {
   data: ListWithCards;
@@ -14,26 +15,42 @@ interface ListItemProps {
 }
 
 export const ListItem = ({ data, index }: ListItemProps) => {
-  const textariaRef = useRef<ElementRef<'textarea'>>(null);
+  const textariaRef = useRef<ElementRef<"textarea">>(null);
 
-  const [isEditing, setIsEditing] = useState(false)
+  const [isEditing, setIsEditing] = useState(false);
 
   const disabledEditing = () => {
-    setIsEditing(false)
-  }
+    setIsEditing(false);
+  };
 
   const enableEditing = () => {
-    setIsEditing(true)
+    setIsEditing(true);
     setTimeout(() => {
-      textariaRef.current?.focus()
-    })
-  }
+      textariaRef.current?.focus();
+    });
+  };
 
   return (
-    <li className='shrink-0 h-full w-[272px] select-none'>
-      <div className='w-full rounded-md bg-[#f1f2f4] shadow-md pb-2'>
+    <li className="shrink-0 h-full w-[272px] select-none">
+      <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
         <ListHeader onAddCard={enableEditing} data={data} />
-        <CardForm listId={data?.id} ref={textariaRef} isEditing={isEditing} enableEditing={enableEditing} disabledEditing={disabledEditing} />
+        <ol
+          className={cn(
+            "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
+            data.cards.length > 0 ? "mt-2" : "mt-0"
+          )}
+        >
+          {data?.cards.map((card, index) => (
+            <CardItem index={index} key={card.id} data={card} />
+          ))}
+        </ol>
+        <CardForm
+          listId={data?.id}
+          ref={textariaRef}
+          isEditing={isEditing}
+          enableEditing={enableEditing}
+          disabledEditing={disabledEditing}
+        />
       </div>
     </li>
   );
