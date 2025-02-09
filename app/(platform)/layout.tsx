91c26554fded23e05ab7706487e72efac58bc9eb
Alexander Tsotsko <alex.tsotsko21@gmail.com>
2025-02-09 14:00:52 +0200

    `feat: Add card modal functionality and provider to layout`

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,11 +1,16 @@
-import { ClerkProvider } from '@clerk/nextjs';
-import { Toaster } from 'sonner';
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
-  return <ClerkProvider>
-    <Toaster/>
-    {children}
-    </ClerkProvider>;
+  return (
+    <ClerkProvider>
+      <Toaster />
+      <ModalProvider />
+      {children}
+    </ClerkProvider>
+  );
 };
 
 export default PlatformLayout;
