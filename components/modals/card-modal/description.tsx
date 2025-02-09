25ab04959d57cb4413b7f961b0d03a56f49cddc4
Alexander Tsotsko <alex.tsotsko21@gmail.com>
2025-02-09 23:54:19 +0200

    feat: Implement editable description in card modal with keyboard and click outside handling

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -1,14 +1,71 @@
 "use client";
 
+import { useState, useRef, ElementRef } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { AlignLeft } from "lucide-react";
+
 import { Skeleton } from "@/components/ui/skeleton";
 import { CardWithList } from "@/types";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
 
 interface DescriptionProps {
   data: CardWithList;
 }
 
 export const Description = ({ data }: DescriptionProps) => {
-  return <div>{data?.description}</div>;
+  const queryClient = useQueryClient();
+  const params = useParams();
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const textareaRef = useRef<ElementRef<"textarea">>(null);
+  const formRef = useRef<ElementRef<"form">>(null);
+
+  const enabledEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
+    });
+  };
+
+  const disabledEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      disabledEditing();
+      textareaRef.current?.blur();
+      e.preventDefault();
+    }
+  };
+
+  useEventListener("keydown", onKeyDown);
+  useOnClickOutside(formRef, disabledEditing);
+
+  const onSubmit = (formData: FormData) => {
+    const description = formData.get("description") as string;
+    const boardId = params.boardId as string;
+
+    // ! TODO: Execute 
+  }
+
+  return (
+    <div className="flex items-start gap-x-3 w-full">
+      <AlignLeft className="h-5 w-5 mt-0.5 text-neutral-700" />
+      <div className="w-full">
+        <p className="font-semibold  text-neutral-700 mb-2">Description</p>
+        <div
+          role="button"
+          className="min-h-[78px] bg-neutral-200 text-sm font-medium py-3 px-3.5 rounded-md"
+        >
+          {data.description ?? "Add a more detailed description..."}
+        </div>
+      </div>
+      {data?.description}
+    </div>
+  );
 };
 
 Description.Skeleton = function DescriptionSkeleton() {
